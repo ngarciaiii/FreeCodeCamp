773071e25601caea262dc0e10ac7198f209fd532
nicolas.garciaiii@gmail.com <nicolas.garciaiii@gmail.com>
2016-06-04 12:28:40 -0400

    edited img

---
+++ Project_2/js/main.js
@@ -0,0 +1,15 @@
+// Tooltip
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+    $('#nav').localScroll(800);
+	$('#intro').parallax("50%", 0.1);
+	$('#second').parallax("50%", 0.1);
+	$('.bg').parallax("50%", 0.4);
+	$('#third').parallax("50%", 0.3);
+});
+	
+	//.parallax(xPosition, speedFactor, outerHeight) options:
+	//xPosition - Horizontal position of the element
+	//inertia - speed to move relative to vertical scroll. Example: 0.1 is one tenth the speed of scrolling, 2 is twice the speed of scrolling
+	//outerHeight (true/false) - Whether or not jQuery should use it's outerHeight option to determine when a section is in the viewport
+	
