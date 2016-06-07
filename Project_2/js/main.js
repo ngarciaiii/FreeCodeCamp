de9353866320969cb7b96075e4fce0c7a489b7ca
nicolas.garciaiii@gmail.com <nicolas.garciaiii@gmail.com>
2016-06-07 00:00:16 -0400

    changed wallapper and added resume

--- Project_2/js/main.js
+++ Project_2/js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     $('#nav').localScroll(800);
 	$('#intro').parallax("50%", 0.1);
 	$('#second').parallax("50%", 0.1);
-	$('.bg').parallax("50%", 0.4);
+	// $('.bg').parallax("50%", 0.4);
 	$('#third').parallax("50%", 0.3);
 });
 	
