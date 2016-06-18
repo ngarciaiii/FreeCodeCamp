f112d3b9772cceb16021f0bb35db9cf104c85e3f
nicolas.garciaiii@gmail.com <nicolas.garciaiii@gmail.com>
2016-06-18 19:16:46 -0400

    change absolute url to relative url

--- Project_2/js/main.js
+++ Project_2/js/main.js
@@ -1,10 +1,10 @@
 // Tooltip
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();
-    $('#nav').localScroll(800);
-	$('#intro').parallax("50%", 0.1);
-	$('#second').parallax("50%", 0.1);
-	$('#third').parallax("50%", 0.3);
+    // $('#nav').localScroll(800);
+	// $('#intro').parallax("50%", 0.1);
+	// $('#second').parallax("50%", 0.1);
+	// $('#third').parallax("50%", 0.3);
 
 });
 	
