6450d3350f5d79902f5b0243eb2953add3ac3186
nicolas.garciaiii@gmail.com <nicolas.garciaiii@gmail.com>
2016-06-30 18:02:11 -0400

    fix mobile friendly, but parallax scroll not working

--- Project_2/js/main.js
+++ Project_2/js/main.js
@@ -5,8 +5,9 @@ $(document).ready(function(){
 	// $('#intro').parallax("50%", 0.1);
 	// $('#second').parallax("50%", 0.1);
 	// $('#third').parallax("50%", 0.3);
-
 });
+
+
 	
 	//.parallax(xPosition, speedFactor, outerHeight) options:
 	//xPosition - Horizontal position of the element
