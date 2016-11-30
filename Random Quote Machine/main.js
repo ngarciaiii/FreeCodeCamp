79057805c7656162f56302c753cf2060257345df
ngarciaiii <nicolas.garciaiii@gmail.com>
2016-11-29 20:42:57 -0500

    add api in js file

--- Random Quote Machine/main.js
+++ Random Quote Machine/main.js
@@ -1,11 +1,21 @@
+//color changes on click
+
 var colors = [ '#F49AC2', '#CB99C9', '#E84751', '#C23B22', '#FFD1DC', '#DEA5A4', '#AEC6CF', '#77DD77', 
 			   '#CFCFC4', '#B39EB5', '#FFB347', '#B19CD9', '#FF6961', '#03C03C', '#FDFD96', '#836953',  
 			   '#779ECB', '#966FD6'], i = 0;
 
 
+$(document).ready(function() {
 
-$("#button").click(function(){ 
-    $("body").css("backgroundColor", colors[i]);
-    $("button").css("backgroundColor", colors[i]);
-    i = (i==colors.length-1) ? 0 : (i+1);
-});
+	$("button").on("click", function(){
+		$.getJSON("https://api.whatdoestrumpthink.com/api/v1/quotes/random", function(data, status){
+			var quote = data.message;
+			alert(quote);
+		});
+		
+	    $("body").css("backgroundColor", colors[i]);
+	    $("button").css("backgroundColor", colors[i]);
+	    i = (i == colors.length-1) ? 0 : (i+1);	
+ 	});
+	
+}); 
