0b6c0c106a1e07ed65df133609da3d2b4b1e2459
ngarciaiii <nicolas.garciaiii@gmail.com>
2016-11-30 23:57:09 -0500

    add google fonts

--- Random Quote Machine/main.js
+++ Random Quote Machine/main.js
@@ -1,8 +1,7 @@
 //color changes on click
 
-var colors = [ '#F49AC2', '#CB99C9', '#E84751', '#C23B22', '#FFD1DC', '#DEA5A4', '#AEC6CF', '#77DD77', 
-			   '#CFCFC4', '#B39EB5', '#FFB347', '#B19CD9', '#FF6961', '#03C03C', '#FDFD96', '#836953',  
-			   '#779ECB', '#966FD6'], i = 0;
+var colors = [ '#00004C','#E50000', '#000066', '#CE0000', '#00007F', '#B70000', '#000099', '#A00000', '#0000B2', 
+			   '#890000', '#0000CC'], i = 0;
 
 
 $(document).ready(function() {
@@ -10,9 +9,9 @@ $(document).ready(function() {
 	$("button").on("click", function(){
 		$.getJSON("https://api.whatdoestrumpthink.com/api/v1/quotes/random", function(data, status){
 			var quote = data.message;
-			alert(quote);
+			$("#message").text(quote);
 		});
-		
+			
 	    $("body").css("backgroundColor", colors[i]);
 	    $("button").css("backgroundColor", colors[i]);
 	    i = (i == colors.length-1) ? 0 : (i+1);	
