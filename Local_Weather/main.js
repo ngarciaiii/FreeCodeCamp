87820cea9e2e93978cb3420a6629c5c5a6ead226
ngarciaiii <nicolas.garciaiii@gmail.com>
2017-02-03 02:24:39 -0500

    update html/css/js fix toggle

---
+++ Local_Weather/main.js
@@ -0,0 +1,37 @@
+var API_KEY = "d19f6725b4e71d3942c11562d40eb174";
+var cel = false;
+var wd;
+
+$(document).ready(function() {
+
+	function displayTemp(ftemp, c) {
+		if (c) return Math.round((ftemp - 32) * (5/9)) + "&#176;";
+		return Math.round(ftemp) + "&#176;"; 
+	}
+
+	$.getJSON("http://ipinfo.io/json", function(d){
+		alert("assigning the data...")
+		loc = d.loc.split(",");
+		alert(loc);
+
+		$.getJSON('http://api.openweathermap.org/data/2.5/weather?units=imperial&lat=' + loc[0] + '&lon=' + loc[1] + '6&APPID=' + API_KEY, function(apiData){
+			//loc[0] = lat
+			//loc [1] = lon
+			//wd = weather data
+			wd = apiData;
+			alert("got data", wd);
+			var currentLocation = wd.name;
+			var currentWeather = wd.weather[0].description;
+			var currentTemp = displayTemp(wd.main.temp, cel);
+			var high = displayTemp(wd.main.temp_max, cel);
+			var low = displayTemp(wd.main.temp_min, cel);
+
+			$('#location').html(currentLocation);
+			$('#currentTemp').html(currentTemp);
+			$('#currentWeather').html(currentWeather);
+			$('#high-low').html(high + " / " + low);
+
+		})
+	});
+
+});
