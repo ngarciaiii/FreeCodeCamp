4ebfd7504a712d88620e2cb8f64ca9fa7f6851f6
ngarciaiii <nicolas.garciaiii@gmail.com>
2017-03-04 20:08:24 -0500

    add toggle, fix icon animation

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,10 +1,23 @@
 
-function openNav() {
-    document.getElementById("mySidenav").style.width = "130px";
-    document.getElementById("main").style.marginLeft = "130px";
-}
 
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+  // function openNav() {
+  //     document.getElementById("mySidenav").style.width = "130px";
+  //     document.getElementById("main").style.marginLeft = "130px";
+  // }
+  //
+  // function closeNav() {
+  //     document.getElementById("mySidenav").style.width = "0";
+  //     document.getElementById("main").style.marginLeft = "0";
+  // }
+
+function toggleNav(){
+  var elementSide = document.getElementById("mySidenav");
+  var elementMain = document.getElementById("main");
+  if (elementSide.style.width == "130px") {
+    elementSide.style.width = "0px";
+    elementMain.style.marginLeft = "0px";
+  } else {
+    elementSide.style.width = "130px";
+    elementMain.style.marginLeft = "130px"
+  }
 }
