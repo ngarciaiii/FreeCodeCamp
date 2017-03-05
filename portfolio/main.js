6ab63e0ed1271c25e70768bd89dcbb78e3552596
ngarciaiii <nicolas.garciaiii@gmail.com>
2017-03-04 19:11:43 -0500

    start portfolio, try add toggle

---
+++ portfolio/main.js
@@ -0,0 +1,10 @@
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "130px";
+    document.getElementById("main").style.marginLeft = "130px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
