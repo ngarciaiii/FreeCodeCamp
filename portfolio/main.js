291b113c92240ba97f3b85c0632edc082ee37d06
ngarciaiii <nicolas.garciaiii@gmail.com>
2017-03-04 21:51:04 -0500

    add two fa icons, fix spin animation

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,23 +1,15 @@
-
-
-  // function openNav() {
-  //     document.getElementById("mySidenav").style.width = "130px";
-  //     document.getElementById("main").style.marginLeft = "130px";
-  // }
-  //
-  // function closeNav() {
-  //     document.getElementById("mySidenav").style.width = "0";
-  //     document.getElementById("main").style.marginLeft = "0";
-  // }
-
 function toggleNav(){
   var elementSide = document.getElementById("mySidenav");
   var elementMain = document.getElementById("main");
+  var elementIcon = document.getElementById("icon")
+
   if (elementSide.style.width == "130px") {
     elementSide.style.width = "0px";
     elementMain.style.marginLeft = "0px";
+    elementIcon.className = "fa fa-bars"
   } else {
     elementSide.style.width = "130px";
     elementMain.style.marginLeft = "130px"
+    elementIcon.className = "fa fa-arrow-left"
   }
 }
